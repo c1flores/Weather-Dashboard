a658e99477ef50639a804ba190ae7caee1a3e8b8
Christian Flores <c1flores@ucsd.edu>
2022-09-10 17:19:10 -0700

    Add event handlers and sparse styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 //function to take user-input (city name), capitalize, and append capitalized city name to search list
 function createCityList(cityList) {
-  $("city-list").empty();
+  $("#city-list").empty();
 
   //Grab local storage value of user-input city and append bootstrap list-item for animated interactivity on hover
   var cityName = Object.keys(cityList);
@@ -8,17 +8,17 @@ function createCityList(cityList) {
     var cityListEntry = $("<button>");
     cityListEntry.addClass("list-group-item list-group-item-action");
 
-  //Capitalize first letter of each city name word and replace list text
-    var splitStr = cityName[i].toLowerCase.split(" ");
+    //Capitalize first letter of each city name word and replace list text
+    var splitStr = cityName[i].toLowerCase().split(" ");
     for (var j = 0; j < splitStr.length; j++) {
       splitStr[j] =
-        splitStr[j].charAt(0).toUpperCase + splitStr[j].substring(1);
+        splitStr[j].charAt(0).toUpperCase() + splitStr[j].substring(1);
     }
 
     var capitalizedCity = splitStr.join(" ");
     cityListEntry.text(capitalizedCity);
 
-    $("city-list").append(cityListEntry);
+    $("#city-list").append(cityListEntry);
   }
 }
 
@@ -27,23 +27,25 @@ function addCityWeather(city, cityList) {
   createCityList(cityList);
 
   //query for current weather
-  var currentQuery = "https://api.openweathermap.org/data/2.5/weather?&units=imperial&appid=b2ec79033d3c438cc2e35b50d25dac5d&q=" + city;
-  
+  var currentQuery =
+    "https://api.openweathermap.org/data/2.5/weather?&units=imperial&appid=b2ec79033d3c438cc2e35b50d25dac5d&q=" +
+    city;
+
   //query for 5-day forecast
-  var forecastQuery = "https://api.openweathermap.org/data/2.5/forecast?&units=imperial&appid=b2ec79033d3c438cc2e35b50d25dac5d&q=" + city;
+  var forecastQuery =
+    "https://api.openweathermap.org/data/2.5/forecast?&units=imperial&appid=b2ec79033d3c438cc2e35b50d25dac5d&q=" +
+    city;
 
   //variable declaration for future use in API call
-  var longitude;
   var latitude;
+  var longitude;
 
   //first API request to modify data so that current weather is shown on dashboard
   $.ajax({
     url: currentQuery,
     method: "GET"
   })
-
   .then(function(weather) {
-
     console.log(currentQuery);
 
     console.log(weather);
@@ -52,10 +54,12 @@ function addCityWeather(city, cityList) {
     var currentDate = moment();
 
     //variable initialization for creating header element to eventually append current date using Moment.js
-    var displayDate =  $("<h3>");
+    var displayDate = $("<h3>");
 
     $("#city-name").empty();
-    $("#city-name").append(displayDate.text(" (" + currentDate.format("M/D/YYY")+ ")"));
+    $("#city-name").append(
+      displayDate.text("(" + currentDate.format("M/D/YYYY") + ")")
+    );
 
     //variable initialization for showing city name on dashboard
     var city = $("<h3>").text(weather.name);
@@ -63,60 +67,59 @@ function addCityWeather(city, cityList) {
 
     //variable initialization for showing respective weather icon on dashboard
     var weatherIcon = $("<img>");
-    weatherIcon.attr("src", "https://openweathermap.org/img/w/" + weather.weather[0].icon + ".png" );
+    weatherIcon.attr(
+      "src",
+      "https://openweathermap.org/img/w/" + weather.weather[0].icon + ".png"
+    );
 
     $("#current-icon").empty();
-    $("current-icon").append(weatherIcon);
+    $("#current-icon").append(weatherIcon);
 
-    $("current-temp").text("Temperature: " + weather.main.temp + " F");
-    $("current-humidity").text("Humidity: " + weather.main.humidity + " %");
-    $("current-wind").text("Wind: " + weather.wind.speed + " MPH");
+    $("#current-temp").text("Temperature: " + weather.main.temp + " F");
+    $("#current-humidity").text("Humidity: " + weather.main.humidity + " %");
+    $("#current-wind").text("Wind: " + weather.wind.speed + " MPH");
 
     //initialization for coordinate variables with city-specific values
-    longitude = weather.coord.lat;
-    latitude = weather.coord.lon;
+    latitude = weather.coord.lat;
+    longitude = weather.coord.lon;
 
-    
     //query to be used for displaying UV index on dashboard
-    var uvQuery = "https://api.openweathermap.org/data/2.5/uvi/forecast?&units=imperial&appid=b2ec79033d3c438cc2e35b50d25dac5d&q=" +
-    "&lat=" +
-    latitude +
-    "&lon=" +
-    longitude;
-    
+    var uvQuery =
+      "https://api.openweathermap.org/data/2.5/uvi/forecast?&units=imperial&appid=b2ec79033d3c438cc2e35b50d25dac5d&q=" +
+      "&lat=" +
+      latitude +
+      "&lon=" +
+      longitude;
+
     //second API request to modify data so that uv index is displayed for current weather on dashboard
     $.ajax({
       url: uvQuery,
-      method:"GET"
+      method: "GET"
     })
-
-    .then(function(uvIndex) {
+    .then(function (uvIndex) {
       console.log(uvIndex);
 
       //create button to display uv index severity. Setting as "red" by default since summer time typically trends with a high sun intensity.
       var uvIndexDisplay = $("<button>");
       uvIndexDisplay.addClass("btn btn-danger");
 
-      //find specific uv index value 
-      $("current-uv").text("UV Index: ");
-      $("current-uv").append(uvIndexDisplay.text(uvIndex[0].value));
+      //find specific uv index value
+      $("#current-uv").text("UV Index: ");
+      $("#current-uv").append(uvIndexDisplay.text(uvIndex[0].value));
       console.log(uvIndex[0].value);
 
       //third API request to modify data so that forecast is displayed for five consecutive days
       $.ajax({
         url: forecastQuery,
-        method:"GET"
+        method: "GET"
       })
-
       .then(function(forecast) {
-
         console.log(forecastQuery);
 
         console.log(forecast);
 
-        //loop through the forecast list array to show a single forecast entry & time. The API used in this query provides weather data at 3-hour increments. 
-        for (var i = 6; i< forecast.list.length; i += 8) {
-
+        //loop through the forecast list array to show a single forecast entry & time. The API used in this query provides weather data at 3-hour increments.
+        for (var i = 6; i < forecast.list.length; i += 8) {
           //create element to display date
           var forecastDate = $("<h5>");
 
@@ -126,31 +129,84 @@ function addCityWeather(city, cityList) {
           console.log("#forecast-date" + forecastPlacement);
 
           //add date to forecast cards
-          $("forecast-date" + forecastPlacement).empty();
-          $("forecast-date" + forecastPlacement).append(forecastDate.text(currentDate.add(1, "days").format("M/D/YYYY")));
+          $("#forecast-date" + forecastPlacement).empty();
+          $("#forecast-date" + forecastPlacement).append(
+            forecastDate.text(currentDate.add(1, "days").format("M/D/YYYY"))
+          );
 
           //create element to populate with appropriate weather icon
           var forecastSymbol = $("<img>");
 
-          forecastSymbol.attr("src", "https://openweathermap.org/img/w" + forecast.list[i].weather[0].icon + ".png");
+          forecastSymbol.attr(
+            "src",
+            "https://openweathermap.org/img/w/" +
+              forecast.list[i].weather[0].icon +
+              ".png"
+          );
 
-          //adding appropriate weather icon
-          $("forecast-icon" + forecastPlacement).empty();
-          $("forecast-icon" + forecastPlacement).append(forecastSymbol);
+          //add appropriate weather icon
+          $("#forecast-icon" + forecastPlacement).empty();
+          $("#forecast-icon" + forecastPlacement).append(forecastSymbol);
 
           console.log(forecast.list[i].weather[0].icon);
-          
-          //setting relevant data to each forecast card
-          $("#forecast-temp" + forecastPlacement).text("Temp: "+ forecast.list[i].main.temp + " °F");
-          $("#forecast-Humidity" + forecastPlacement).text("Humidity: "+ forecast.list[i].main.humidity + " %");
 
-          $(".forecast").attr("style", "background-color:dodgerblue; color:white");
+          //add relevant data to each forecast card
+          $("#forecast-temp" + forecastPlacement).text(
+            "Temp: " + forecast.list[i].main.temp + " °F"
+          );
+          $("#forecast-humidity" + forecastPlacement).text(
+            "Humidity: " + forecast.list[i].main.humidity + " %"
+          );
 
+          $(".forecast").attr(
+            "style",
+            "background-color:dodgerblue; color:white"
+          );
         }
       });
     });
   });
 }
 
+$(document).ready(function () {
+  var cityListString = localStorage.getItem("cityList");
 
+  var cityList = JSON.parse(cityListString);
 
+  if (cityList == null) {
+    cityList = {};
+  }
+
+  createCityList(cityList);
+
+  $("#current-weather").hide();
+  $("#forecast-weather").hide();
+
+  $("#search-button").on("click", function(event) {
+    event.preventDefault();
+
+    var city = $("#city-input").val().trim().toLowerCase();
+
+    if (city != "") {
+      cityList[city] = true;
+
+      localStorage.setItem("cityList", JSON.stringify(cityList));
+
+      addCityWeather(city, cityList);
+
+      $("#current-weather").show();
+      $("#forecast-weather").show();
+    }
+  });
+
+  $("#city-list").on("click", "button", function(event) {
+    event.preventDefault();
+
+    var city = $(this).text();
+
+    addCityWeather(city, cityList);
+
+    $("#current-weather").show();
+    $("#forecast-weather").show();
+  });
+});
