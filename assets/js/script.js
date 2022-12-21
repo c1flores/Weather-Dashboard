c5cb599687734f9072bb6a07de8da77639f9d4e3
Christian Flores <c1flores@ucsd.edu>
2022-12-20 21:58:32 -0800

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-//function to take user-input (city name), capitalize, and append capitalized city name to search list
+//Function to take user-input (city name), capitalize, and append capitalized city name to search list
 function createCityList(cityList) {
   $("#city-list").empty();
 
@@ -22,25 +22,25 @@ function createCityList(cityList) {
   }
 }
 
-//main function to populate dashboard with city weather data for both current weather and 5-day forecast
+//Main function to populate dashboard with city weather data for both current weather and 5-day forecast
 function addCityWeather(city, cityList) {
   createCityList(cityList);
 
-  //query for current weather
+  //Query for current weather
   var currentQuery =
     "https://api.openweathermap.org/data/2.5/weather?&units=imperial&appid=b2ec79033d3c438cc2e35b50d25dac5d&q=" +
     city;
 
-  //query for 5-day forecast
+  //Query for 5-day forecast
   var forecastQuery =
     "https://api.openweathermap.org/data/2.5/forecast?&units=imperial&appid=b2ec79033d3c438cc2e35b50d25dac5d&q=" +
     city;
 
-  //variable declaration for future use in API call
+  //Variable declaration for future use in API call
   var latitude;
   var longitude;
 
-  //first API request to modify data so that current weather is shown on dashboard
+  //First API request to modify data so that current weather is shown on dashboard
   $.ajax({
     url: currentQuery,
     method: "GET",
@@ -49,10 +49,10 @@ function addCityWeather(city, cityList) {
 
     console.log(weather);
 
-    //variable initialization to display current day on current date section
+    //Variable initialization to display current day on current date section
     var currentDate = moment();
 
-    //variable initialization for creating header element to eventually append current date using Moment.js
+    //Variable initialization for creating header element to eventually append current date using Moment.js
     var displayDate = $("<h3>");
 
     $("#city-name").empty();
@@ -60,11 +60,11 @@ function addCityWeather(city, cityList) {
       displayDate.text("(" + currentDate.format("M/D/YYYY") + ")")
     );
 
-    //variable initialization for showing city name on dashboard
+    //Variable initialization for showing city name on dashboard
     var city = $("<h3>").text(weather.name);
     $("#city-name").prepend(city);
 
-    //variable initialization for showing respective weather icon on dashboard
+    //Variable initialization for showing respective weather icon on dashboard
     var weatherIcon = $("<img>");
     weatherIcon.attr(
       "src",
@@ -78,11 +78,11 @@ function addCityWeather(city, cityList) {
     $("#current-humidity").text("Humidity: " + weather.main.humidity + " %");
     $("#current-wind").text("Wind: " + weather.wind.speed + " MPH");
 
-    //initialization for coordinate variables with city-specific values
+    //Initialization for coordinate variables with city-specific values
     latitude = weather.coord.lat;
     longitude = weather.coord.lon;
 
-    //query to be used for displaying UV index on dashboard
+    //Query to be used for displaying UV index on dashboard
     var uvQuery =
       "https://api.openweathermap.org/data/2.5/uvi/forecast?&units=imperial&appid=b2ec79033d3c438cc2e35b50d25dac5d&q=" +
       "&lat=" +
@@ -90,14 +90,14 @@ function addCityWeather(city, cityList) {
       "&lon=" +
       longitude;
 
-    //second API request to modify data so that uv index is displayed for current weather on dashboard
+    //Second API request to modify data so that uv index is displayed for current weather on dashboard
     $.ajax({
       url: uvQuery,
       method: "GET",
     }).then(function (uvIndex) {
       console.log(uvIndex);
 
-      //create button to display uv index severity. Conditional statement for granular ranking of severity
+      //Create button to display uv index severity. Conditional statement for granular ranking of severity
       var uvIndexDisplay = $("<button>");
       
       if(uvIndex[0].value > 0 && uvIndex[0].value < 2 ) {
@@ -112,12 +112,12 @@ function addCityWeather(city, cityList) {
         uvIndexDisplay.addClass("btn btn-danger");
       }
 
-      //find specific uv index value
+      //Find specific uv index value
       $("#current-uv").text("UV Index: ");
       $("#current-uv").append(uvIndexDisplay.text(uvIndex[0].value));
       console.log(uvIndex[0].value);
 
-      //third API request to modify data so that forecast is displayed for five consecutive days
+      //Third API request to modify data so that forecast is displayed for five consecutive days
       $.ajax({
         url: forecastQuery,
         method: "GET",
@@ -126,23 +126,23 @@ function addCityWeather(city, cityList) {
 
         console.log(forecast);
 
-        //loop through the forecast list array to show a single forecast entry & time. The API used in this query provides weather data at 3-hour increments.
+        //Loop through the forecast list array to show a single forecast entry & time. The API used in this query provides weather data at 3-hour increments.
         for (var i = 6; i < forecast.list.length; i += 8) {
           //create element to display date
           var forecastDate = $("<h5>");
 
-          //use loop interval to provide an index so that days are placed in correct order
+          //Use loop interval to provide an index so that days are placed in correct order
           var forecastPlacement = (i + 2) / 8;
 
           console.log("#forecast-date" + forecastPlacement);
 
-          //add date to forecast cards
+          //Add date to forecast cards
           $("#forecast-date" + forecastPlacement).empty();
           $("#forecast-date" + forecastPlacement).append(
             forecastDate.text(currentDate.add(1, "days").format("M/D/YYYY"))
           );
 
-          //create element to populate with appropriate weather icon
+          //Create element to populate with appropriate weather icon
           var forecastSymbol = $("<img>");
 
           forecastSymbol.attr(
@@ -152,13 +152,13 @@ function addCityWeather(city, cityList) {
               ".png"
           );
 
-          //add appropriate weather icon
+          //Add appropriate weather icon
           $("#forecast-icon" + forecastPlacement).empty();
           $("#forecast-icon" + forecastPlacement).append(forecastSymbol);
 
           console.log(forecast.list[i].weather[0].icon);
 
-          //add relevant data to each forecast card
+          //Add relevant data to each forecast card
           $("#forecast-temp" + forecastPlacement).text(
             "Temp: " + forecast.list[i].main.temp + " °F"
           );
@@ -179,7 +179,7 @@ function addCityWeather(city, cityList) {
   });
 }
 
-//ready event after DOM has been loaded
+//Ready event after DOM has been loaded
 $(document).ready(function () {
   var cityListString = localStorage.getItem("cityList");
 
@@ -194,7 +194,7 @@ $(document).ready(function () {
   $("#current-weather").hide();
   $("#forecast-weather").hide();
 
-  //event handler for search results to prompt program functionality
+  //Event handler for search results to prompt program functionality
   $("#search-button").on("click", function (event) {
     event.preventDefault();
 
@@ -212,7 +212,7 @@ $(document).ready(function () {
     }
   });
 
-  //event handler for choosing previously searched city and populating with respective weather data
+  //Event handler for choosing previously searched city and populating with respective weather data
   $("#city-list").on("click", "button", function (event) {
     event.preventDefault();
 
